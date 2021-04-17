52647562994e410bca59262a441e7460516a27fc
cpiero-a <cpiero-a@student.42sp.org.br>
2021-04-17 07:42:40 +0000

    amei 42

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpiero-a <cpiero-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 18:27:16 by cpiero-a          #+#    #+#             */
+/*   Updated: 2021/04/16 20:42:53 by cpiero-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		++i;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		++i;
+		++j;
+	}
+	dest[i] = 0;
+	return (dest);
+}
